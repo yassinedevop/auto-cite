c93fee1dfda1ee92459e3c133f811c1ddecd668c
yassine <yassineseghr17@gmail.com>
2023-07-06 17:59:37 +0100

    Update script.js
    added documentation

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+/*
+ce script remplace les liens dans votre document sur google docs
+par des citations
+*/
 function myFunction() {
   var doc = DocumentApp.getActiveDocument();
   var body = doc.getBody();
   var paragraphs = body.getParagraphs();
   var j = 0;
 
-  for(var i = paragraphs.length - 75 ; i < paragraphs.length ; i++){
+  for(var i = paragraphs.length - 75
+  /*
+  à remplacer par l'index du premier lien
+  par exemple pour moi j'ai les liens à la fin du document
+  et j'ai 75 liens pour cela j'ai indexé i sur le dernier paragraphe - 75
+*/
+ ; i < paragraphs.length ; i++){
     j++;
     var text = paragraphs[i].getText();
     var link = text.split(" ");
